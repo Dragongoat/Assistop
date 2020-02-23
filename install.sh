a80fc073d67a90ce9222a6dd0924ceaa6b66234d
Dragongoat <japawek@gmail.com>
2020-02-23 13:06:41 -0800

    Install script v1

---
+++ install.sh
@@ -0,0 +1,35 @@
+# Link to install RaspAP: https://howtoraspberrypi.com/create-a-wi-fi-hotspot-in-less-than-10-minutes-with-pi-raspberry/
+# Free up the wireless interface
+echo 'Freeing up wireless interface. Previous configuration saved at /etc/wpa_supplicant.conf.sav.'
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.sav
+sudo cp ./templates/wpa_supplicant.conf.template /etc/wpa_supplicant/wpa_supplicant.conf
+
+# Install hostapd and RaspAP
+echo 'Installing hostapd and RaspAP.'
+wget -q https://git.io/voEUQ -O /tmp/raspap && bash /tmp/raspap
+
+echo 'Configuring network interfaces to static.'
+# Set static IP for eth0 and wlan0
+sudo cp ./templates/interfaces.template /etc/network/interfaces
+
+# Set eth0 static in raspAP files
+sudo cp ./templates/eth0.ini.template /etc/raspap/networking/eth0.ini 
+
+# Set wlan0 static in raspAP files (10.3.141.1 to work with default RaspAP DHCP setup)
+sudo cp ./templates/wlan0.ini.template /etc/raspap/networking/wlan0.ini
+
+echo 'Renaming device.'
+# Make sure device can use hostname.local
+sudo apt update -y -qq
+sudo apt upgrade -y -qq
+sudo apt install -y -qq avahi-daemon
+
+# Set hostname to assistop
+sudo sed -i 's/127\.0\.1\.1[[:blank:]]*raspberrypi/127.0.1.1\tassistop/1' /etc/hosts
+sudo cp ./templates/hostname.template /etc/hostname
+
+# Link to install Docker: https://linuxhint.com/install_docker_on_raspbian_os/
+
+# Link to install Docker-compose: https://stackoverflow.com/questions/58747879/docker-compose-usr-local-bin-docker-compose-line-1-not-command-not-found
+
+# Link to install Django: https://realpython.com/django-setup/
