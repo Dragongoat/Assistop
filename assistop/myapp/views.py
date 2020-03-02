b1a5cbdcc7e2928fcb46028b5a947f8dc9cc90fc
Dragongoat <japawek@gmail.com>
2020-03-02 02:01:43 +0000

    Added skeleton webpage using foundation.

--- assistop/myapp/views.py
+++ assistop/myapp/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+	context={
+		"title": "Assistop: Dashboard",
+	}
+	return render(request, "index.html", context = context)
+
+def assistants(request):
+    context={
+        "title": "Assistop: My Assistants",
+    }
+    return render(request, "assistants.html", context=context)
+
+def devices(request):
+    context={
+        "title": "Assistop: My Devices",
+    }
+    return render(request, "devices.html", context=context)
+
+def schedule(request):
+    context={
+        "title": "Assistop: Schedule",
+    }
+    return render(request, "schedule.html", context=context)
+
+def notifications(request):
+    context={
+        "title": "Assistop: Notifications",
+    }
+    return render(request, "notifications.html", context=context)
+
+def hotspot(request):
+    context={
+        "title": "Assistop: Hotspot",
+    }
+    return render(request, "hotspot.html", context=context)
+
+def system(request):
+    context={
+        "title": "Assistop: System",
+    }
+    return render(request, "system.html", context=context)
+
+def about(request):
+    context={
+        "title": "Assistop: About",
+    }
+    return render(request, "about.html", context=context)
